a2fbe1d1535beff321ced5a9b090f3702d418753
ryufloat <ryu.mochi@gmail.com>
2014-05-07 22:12:42 +0900

    Fix install.sh more comfortable

--- install.sh
+++ install.sh
@@ -1,5 +1,39 @@
 #!bin/sh
+##-----------------------------------------------
+#  Install NEOBundle
+##-----------------------------------------------
 
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+if [ -e /usr/bin/curl ] || [ -e /bin/curl]; then
+    curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+else
+    echo "You should install 'curl' due to install NEOBunbdle."
+    exit
+fi
 
-ln -is $PWD/.vimrc $HOME
+##-----------------------------------------------
+
+
+##-----------------------------------------------
+#  Put dotfiles
+##-----------------------------------------------
+
+dir=$HOME/dotfiles
+olddir=$HOME/dotfiles_old
+files="vimrc"
+
+echo "Creating $olddir for backup of any existing dotfiles in $HOME"
+mkdir -p $olddir
+echo "...done"
+
+echo "Chenge to the $dir directory"
+cd $dir
+echo "...done"
+
+for file in $files; do
+    echo "Moving any existing dotfiles from $HOME to $olddir"
+    mv $HOME/.$file $olddir/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/.$file $HOME/.$file
+done
+
+##-----------------------------------------------
