d0b7bd37c22e1240773afb8dbe067cac9f4a4724
ryutamaki <ryu.mochi@gmail.com>
2014-09-10 01:49:20 +0900

    Install homebrew and homebrew packages with "install_brew_packages.sh"

---
+++ install_brew_packages.sh
@@ -0,0 +1,11 @@
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+
+# Priority
+brew install zsh
+brew install git
+brew install tmux
+brew install vim
+brew install tig
+
+# For mac
+brew install reattach-to-user-namespac
