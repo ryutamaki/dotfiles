6ac3b9bda67d25925ca2e0b09eb1e265ea830ac8
ryutamaki <ryu.mochi@gmail.com>
2015-02-02 01:08:45 +0900

    Initialize pumice

---
+++ .zsh/pumice.zsh
@@ -0,0 +1,12 @@
+source $HOME/.zsh/pumice/pumice.zsh
+
+# zsh-syntax-highlighting
+pumice bundle zsh-users/zsh-syntax-highlighting zsh-syntax-highlighting.zsh
+
+# zsh-completions
+pumice bundle zsh-users/zsh-completions src
+
+# cdd
+pumice bundle m4i/cdd cdd
+typeset -ga chpwd_functions
+chpwd_functions+=_cdd_chpwd
--- .zshrc
+++ .zshrc
@@ -4,3 +4,11 @@
 if [ -e ~/.zsh/zshrc ]; then
     source ~/.zsh/zshrc
 fi
+
+
+##------------------------------------------------
+#  include .zsh/pumice.zsh
+##------------------------------------------------
+if [ -e ~/.zsh/pumice.zsh ]; then
+    source ~/.zsh/pumice.zsh
+fi
