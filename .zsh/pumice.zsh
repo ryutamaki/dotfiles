1ab9e2dd525dd9427f2f33aebeabf513a7afc431
ryutamaki <ryu.mochi@gmail.com>
2015-05-27 02:27:20 +0900

    Add a plugin k

--- .zsh/pumice.zsh
+++ .zsh/pumice.zsh
@@ -5,7 +5,11 @@ pumice bundle zsh-users/zsh-syntax-highlighting zsh-syntax-highlighting.zsh
 
 # zsh-completions
 pumice fpath zsh-users/zsh-completions src
+
 # cdd
 pumice bundle m4i/cdd cdd
 typeset -ga chpwd_functions
 chpwd_functions+=_cdd_chpwd
+
+# k
+pumice bundle supercrabtree/k k.sh
