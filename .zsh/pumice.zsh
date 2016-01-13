78a324f25fd7bc4940959062c7c0a8a2af671feb
ryutamaki <ryu.mochi@gmail.com>
2016-01-13 11:06:49 +0900

    Add z and remove cdd

--- .zsh/pumice.zsh
+++ .zsh/pumice.zsh
@@ -6,10 +6,8 @@ pumice bundle zsh-users/zsh-syntax-highlighting zsh-syntax-highlighting.zsh
 # zsh-completions
 pumice fpath zsh-users/zsh-completions src
 
-# cdd
-pumice bundle m4i/cdd cdd
-typeset -ga chpwd_functions
-chpwd_functions+=_cdd_chpwd
+# z
+pumice bundle rupa/z z.sh
 
 # k
 pumice bundle supercrabtree/k k.sh
