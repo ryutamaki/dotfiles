4703001196fb6731a4d26b98e09d08b692ad1dcc
ryu.tamaki <ryutamaki@Mac.localdomain>
2025-04-24 09:19:32 +0900

    feat: Remove packages from brew default install list

--- bin/brewfile.sh
+++ bin/brewfile.sh
@@ -3,14 +3,10 @@ if ! [ `which brew` ]; then
 fi
 
 # Priority
-brew install git
 brew install tmux
-brew install vim
 brew install tig
 brew install lv
-brew install exa
 
 # Sub
-brew install reattach-to-user-namespace
 brew install jq
 brew install ag
